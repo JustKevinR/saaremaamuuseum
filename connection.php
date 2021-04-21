4612a25fdd3d868be063c6a10c13148483f72a52
ratsepmarkus <markusratsep11@gmail.com>
2021-04-21 14:02:12 +0300

    .

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$dbServername = "d83300.mysql.zonevs.eu";
+$dbUsername = "d83300sa325728";
+$dbPassword = "asdfsgdhklkiotiyuhgfbcnv";
+$dbName = "d83300_muuseum";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
