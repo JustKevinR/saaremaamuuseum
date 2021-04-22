54a3e29dddfa91de3ee0999b518cb5fe8d82fad4
Markus Rätsep <56824783+ratsepmarkus@users.noreply.github.com>
2021-04-22 09:34:41 +0300

    Delete connection.php

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbServername = "d83300.mysql.zonevs.eu";
-$dbUsername = "d83300sa325728";
-$dbPassword = "asdfsgdhklkiotiyuhgfbcnv";
-$dbName = "d83300_muuseum";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
