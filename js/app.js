285bea479377384dff3f30b97133fd5bb34d59d6
ratsepmarkus <markusratsep11@gmail.com>
2021-04-22 13:04:21 +0300

    slider/arrays

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var list= {
+    "email":"test@test.com",
+    "phone":"123456789",
+    "events": [
+      { "name":"", "start": "", "end": "", "url": ""},
+      { "name":"", "start": "", "end": "", "url": ""},
+      { "name":"", "start": "", "end": "", "url": ""},
+    ],
+    "open_est": [
+        { "name":"", "start": "", "end": "", "url": ""},
+      ]
+   }
+document.getElementById("email").innerHTML = list["email"];
+document.getElementById("phone").innerHTML = list["phone"];
